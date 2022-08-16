427a83708e42c60e494bea2b9e08423b0794173b
Java project <java100class@gmail.com>
2022-08-16 22:59:54 +0300

    sed

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -1,5 +1,7 @@
 public class GitIgnore {
     public static void main(String[] args) {
         System.out.println("git ignore deneme 1");
+        System.out.println("git ignore deneme 2");
+
     }
 }
